775cf21ff805be6e12d799e91199ab0cc69bf8e3
Oscar Adrian Palacios Cabrera <oapalacios@satelitesupport.com>
2026-03-31 09:12:45 -0500

    fix: se ubica data para generar automaticamente el zip

--- infra/data.tf
+++ infra/data.tf
@@ -1,3 +1,9 @@
 data "aws_s3_bucket" "bronze" {
     bucket = "elite-datalake-bronze-dev"
 }
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "../src"
+  output_path = "../build/lambda.zip"
+}
